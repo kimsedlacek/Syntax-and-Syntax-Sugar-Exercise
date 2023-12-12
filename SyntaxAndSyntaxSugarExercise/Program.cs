85c4149a858bf082eb7f1ee90a3844db7a51a678
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-12 17:13:29 -0500

    Syn and Syn Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 10;
+
+            
+
+            var response = "is less than nine";
+            var anotherResponse = "is greater than or equal to nine";
+
+
+            var lessThan = answer < 9 ? $"{answer} {response}" : $"{answer} {anotherResponse}";
+
+            Console.WriteLine(lessThan);
+
+
+            //if (answer < 9)
+            //{
+            //    Console.WriteLine($"{answer} {response}.");
+            //}
+            //else
+            //{
+            //     Console.WriteLine($"{answer} {anotherResponse}.");
+            //}
+
+
+
+
+            //var synSug = (answer < 9) ? response : anotherResponse;
+
+            //Console.WriteLine($"{answer} {synSug});
+
+
+
+
+
+
+            //Console.WriteLine(answer < 9 ? {answer}  {response} : )
+            //if (answer < 9)
+            //{
+            //    Console.WriteLine($"{answer} is less than nine");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{answer} is greater than or equal to nine");
+            //}
         }
     }
 }
